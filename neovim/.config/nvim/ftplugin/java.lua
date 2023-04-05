7f56be94cb579695c9381e6a110b3d404d782880
Cuinardium <sballerini@itba.edu.ar>
2023-04-05 11:33:23 -0300

    para gonza

--- neovim/.config/nvim/ftplugin/java.lua
+++ neovim/.config/nvim/ftplugin/java.lua
@@ -48,7 +48,7 @@ local config = {
   cmd = {
 
     -- 💀
-    "java", -- or '/path/to/java11_or_newer/bin/java'
+    "/usr/lib/jvm/java-17-openjdk/bin/java", -- or '/path/to/java11_or_newer/bin/java'
     -- depends on if `java` is in your $PATH env variable and if it points to the right version.
 
     "-Declipse.application=org.eclipse.jdt.ls.core.id1",
