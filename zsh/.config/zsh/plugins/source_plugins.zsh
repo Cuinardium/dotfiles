93fb675940b593fa0a8ae654e7ac92abb612bcb0
Santiago Ballerini <sballerini@itba.edu.ar>
2023-06-30 19:23:53 -0300

    add https://github.com/gh0stzk/dotfiles files

--- zsh/.config/zsh/plugins/source_plugins.zsh
+++ zsh/.config/zsh/plugins/source_plugins.zsh
@@ -5,6 +5,6 @@ source ~/.config/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source ~/.config/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 # Autocomplete
-zstyle ':autocomplete:*' min-delay 5
-source ~/.config/zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+# zstyle ':autocomplete:*' min-delay 5
+# source ~/.config/zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh
 
