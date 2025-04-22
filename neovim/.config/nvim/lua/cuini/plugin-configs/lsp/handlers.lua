1783a686eaae08495aa15af7f39710f480777d52
Santiago Ballerini <sballerini@itba.edu.ar>
2025-04-21 23:31:46 -0300

    fix: fix handler config

--- neovim/.config/nvim/lua/cuini/plugin-configs/lsp/handlers.lua
+++ neovim/.config/nvim/lua/cuini/plugin-configs/lsp/handlers.lua
@@ -5,23 +5,18 @@ local M = {}
 
 -- TODO: backfill this to template
 M.setup = function()
-    local signs = {
-        { name = "DiagnosticSignError", text = "" },
-        { name = "DiagnosticSignWarn", text = "" },
-        { name = "DiagnosticSignHint", text = "" },
-        { name = "DiagnosticSignInfo", text = "" },
-    }
-
-    for _, sign in ipairs(signs) do
-        vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-    end
 
     local config = {
         -- disable virtual text
         virtual_text = false,
         -- show signs
         signs = {
-            active = signs,
+            text = {
+                [vim.diagnostic.severity.ERROR] = "",
+                [vim.diagnostic.severity.WARN] = "",
+                [vim.diagnostic.severity.HINT] = "",
+                [vim.diagnostic.severity.INFO] = ""
+            }
         },
         update_in_insert = true,
         underline = true,
@@ -57,7 +52,7 @@ local function lsp_highlight_document(client)
         autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
         autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
       augroup END
-    ]]       ,
+    ]],
             false
         )
     end
@@ -76,9 +71,9 @@ local function lsp_keymaps(bufnr)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>lc", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>lp", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>'
-        , opts)
+    , opts)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ln", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>'
-        , opts)
+    , opts)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>lr", '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
     vim.cmd [[ command! Format execute 'lua vim.lsp.buf.format()' ]]
@@ -92,7 +87,6 @@ M.on_attach = function(client, bufnr)
         navic.attach(client, bufnr)
         navbuddy.attach(client, bufnr)
     end
-
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
